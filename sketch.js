1a8b2d249885a3a051264ab549e7b2410424efb1
Tanvi999 <70043541+Tanvi999@users.noreply.github.com>
2020-09-19 18:55:08 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+var car, wall, speed, weight;
+
+function setup() {
+ createCanvas(1600,400);
+  
+ car = createSprite(50, 200, 50, 50);
+
+ wall = createSprite(1500, 200, 20, height/2);
+
+ speed = random(55,90);
+
+ weight = random(400,1500);
+
+ car.velocityX = speed;
+}
+
+function draw() {
+  background(0);  
+
+ if(wall.x-car.x <= wall.width/2 + car.width/2)
+ {
+  car.velocityX = 0;
+  var deform = 0.5*weight*speed*speed/22500
+  if(deform < 80)
+  {
+   car.shapeColor = color(0,255,0);
+  }
+  else if(deform > 80 && deform < 180)
+  {
+   car.shapeColor = "yellow";
+  }
+  else if(deform > 180)
+  {
+   car.shapeColor = color(255,0,0);
+  }
+ }
+
+ drawSprites();
+}
+
+
+
+
